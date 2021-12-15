7e17edb2cbe8ecc38799b08ca7f5b229acc0e682
Lucas Borges - Dev <drumerlucas@gmail.com>
2021-12-15 00:19:51 -0300

    Categories CRUD flux

--- src/application/services/categories.service.ts
+++ src/application/services/categories.service.ts
@@ -5,4 +5,20 @@ export class CategoriesService {
     create(){
         return "This method create an category";
     }
+
+    list(){
+        return "This method list only one category";
+    }
+
+    listAll(){
+        return "This method list all categories";
+    }
+
+    update(){
+        return "This method updates an category";
+    }
+
+    delete(){
+        return "This method deletes an category"
+    }
 }
